7ac6ef127484585486956ea091e5c1fab2b42743
Robin Dalton (PACTERA TECHNOLOGIES INC) <v-rodalt@microsoft.com>
2019-11-07 15:04:10 -0800

    restore backup method

--- src/app/tabmore/components/import/import.component.ts
+++ src/app/tabmore/components/import/import.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { File } from '@ionic-native/File/ngx';
 
 @Component({
   selector: 'quizcards-import',
@@ -6,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./import.component.scss'],
 })
 export class ImportComponent implements OnInit {
+  backups = [];
 
-  constructor() { }
+  constructor(private File: File) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loadBackups('');
+  }
 
+  async loadBackups(dir) {    
+    await this.File.listDir(this.File.externalRootDirectory, dir)
+      .then(entries => {
+
+        for(let i = 0; i < entries.length; i++) {
+          if(!entries[i]['isDirectory']) {
+            const qcs_index = entries[i]['fullPath'].lastIndexOf('.qcs');
+            if(qcs_index > -1 && qcs_index + 4 === entries[i]['fullPath'].length) {
+              this.backups.push(entries[i]);
+            }
+          } else {
+            // var children_data = await this.listDirHelper(entries[i]['nativeURL'],entries[i]['name']);
+            // if(children_data !== false){
+            //   current_folder['children'].push(children_data);
+            // }
+          }
+        }
+      })
+      .catch((err) => {
+          console.log('error in listing directory', err);
+      });
+  }
+
+  restoreBackup(b) {
+    
+  }
 }
